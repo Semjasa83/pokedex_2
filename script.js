e113090b849e1e88c53e2687c201c464af4c01f8
MarceldZ <94352026+Semjasa83@users.noreply.github.com>
2022-09-14 14:19:15 +0200

    load of pokemon works, interval failed

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 let allPokemon = [];
 let allPokemonDetail = [];
-let pokeId = 900;
+let pokeId = 50;
 
-
+/**
+ * fetches the Main Path from API for all Pokemon
+ */
 
 async function loadPokemon() {
     document.getElementById('data').innerHTML = '';
     await loadPokemonIndex();
     console.log('first array', allPokemon);
-    //loadPokemonDetail();
 }
 
+/**
+ * Push every single Pokemon as JSON in allPokeDetail
+ */
 async function loadPokemonIndex() {
     for (let i = 1; i <= pokeId; i++) {
         const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
@@ -20,19 +24,20 @@ async function loadPokemonIndex() {
         allPokemonDetail = allPokemon;
         console.log(i);
         if (i == 20) {
-            loadPokemonDetail()
-        }
+            loadPokemonDetail();
+        } 
     }
 }
 
+/**
+ * Render first 20 Pokemon
+ */
 function loadPokemonDetail() {
     for (let i = 0; i < allPokemonDetail.length; i++) {
         let element = allPokemonDetail[i];
         console.log('detail array', element);
         document.getElementById('data').innerHTML += renderPokeData(i);
     }
-
-
 }
 
 
