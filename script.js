f7e333e0e149b118e8c30e05e5bf3fd546395f9e
MarceldZ <94352026+Semjasa83@users.noreply.github.com>
2022-09-15 14:57:54 +0200

    scroll load and render works

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let allPokemon = [];
 let allPokemonDetail = [];
-let pokeId = 898;
-let start = 1;
-let stop = 20;
+//let pokeId = 898;
+let startOffset = 1; // 1 + 40 = 41      41 + 40 = 81
+let stopOffset = 40; // 40 + 40 = 80     80 + 40 = 120
+let permissionForLoadMore = true;
 
 /**
  * fetches the Main Path from API for all Pokemon
@@ -18,17 +19,19 @@ async function loadPokemon() {
  * Push every single Pokemon as JSON in allPokeDetail
  */
 async function loadPokemonIndex() {
-    for (let i = start; i <= pokeId; i++) {
+    for (let i = startOffset; i <= stopOffset; i++) {
         const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
         let response = await fetch(url);
         responseAsJson = await response.json();
         allPokemon.push(responseAsJson);
         allPokemonDetail = allPokemon;
-        console.log(i);   //_____CONSOLE
-        if (i == 20) {
-            loadPokemonData();
-        }
+        //console.log(i);   //_____CONSOLE
+         // i % 20 == 0
+    
     }
+    loadPokemonData();
+    console.log(allPokemonDetail);
+    permissionForLoadMore = true;
 }
 
 /**
@@ -39,7 +42,7 @@ function loadPokemonTypes(i) {
     let htmlCode = "";
     for (let j = 0; j < allPokemonDetail[i].types.length; j++) {
         const types = allPokemonDetail[i].types[j];
-        htmlCode += /*hmtl*/`
+        htmlCode += /*html*/`
         <div class="overview-type-container ${types.type['name']}">
         <div>${types.type['name']}</div>
         `
@@ -51,9 +54,9 @@ function loadPokemonTypes(i) {
  * Render first 20 Pokemon
  */
 function loadPokemonData() {
-    for (let i = 0; i < allPokemonDetail.length; i++) {
+    for (let i = startOffset - 1; i < allPokemonDetail.length; i++) {
         let element = allPokemonDetail[i];
-        console.log('detail array', element); //_____CONSOLE
+        //console.log('detail array', element); //_____CONSOLE
         loadPokemonTypes(i);
         document.getElementById('data').innerHTML += renderPokeData(i);
     }
@@ -63,7 +66,7 @@ function loadPokemonData() {
 function renderPokeData(i) {
 
     return /*html*/`
-        <ul >
+        <ul class="test-div">
             <li>
                 id: ${allPokemonDetail[i].id}
             </li>
@@ -74,7 +77,22 @@ function renderPokeData(i) {
                 type: ${loadPokemonTypes(i)}
             </li>
             <img src="${allPokemonDetail[i].sprites.front_default}">
-        </ul>
+        </ul>`
+}
 
- `
+
+//when you are near the bottom of Site --> load more Pokemons 
+function lazyLoading() {
+    if ((window.innerHeight + window.scrollY + 400) >= document.body.offsetHeight && permissionForLoadMore) {
+        permissionForLoadMore = false;
+        loadMorePokemon();
+    }
+}
+
+
+async function loadMorePokemon() {
+    startOffset += 40;
+    stopOffset += 40;
+    await loadPokemonIndex();
+    // loadPokemonData();
 }
